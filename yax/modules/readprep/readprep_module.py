b3261ca8d9da2a0c98aa0bcd3b8b7b1283d4249c
Mike <mike.deberg@gmail.com>
2016-04-04 13:56:54 -0700

    initial addition of readprep module

--- yax/modules/readprep/readprep_module.py
+++ yax/modules/readprep/readprep_module.py
@@ -5,11 +5,19 @@ import subprocess
 import configparser
 
 
-def main(working_dir, output, path_to_readprep_main: File, infile_list: File,
-         num_workers: Int, max_mem_target_gb: Int, trimming_type: Str,
-         trimming_segment_length: Int, adapter_list: File="optional",
-         adapter_tolerance: Int="optional", minimum_quality: Int="optional",
-         max_below_threshold: Int="optional") -> (PreparedReads):
+def main(working_dir,
+         output,
+         path_to_readprep_main: File,
+         infile_list: File,
+         num_workers: Int,
+         max_mem_target_gb: Int,
+         trimming_type: Str,
+         trimming_segment_length: Int,
+         adapter_list: File="optional",
+         adapter_tolerance: Int="optional",
+         minimum_quality: Int="optional",
+         max_below_threshold: Int="optional")\
+         -> PreparedReads:
 
     prepared_reads, = output
 
@@ -27,31 +35,35 @@ def main(working_dir, output, path_to_readprep_main: File, infile_list: File,
 
     path_to_config_file = "/".join([working_dir, "/", "runid",
                                     "readprep_config.ini"])
-    write_readprep_config()
-    run_readprep()
+    write_readprep_config(path_to_config_file)
+    run_readprep(path_to_config_file, path_to_readprep_main)
 
     return output
 
 
-def write_readprep_config():
+def write_readprep_config(path_to_config_file, prepared_reads):
     config = configparser.ConfigParser()
-    config['paths'] = {'infile_list': infile_list,
-                       'work_dir': working_dir,
+    config['paths'] = {'infile_list': prepared_reads.infile_list,
+                       'work_dir': prepared_reads.working_dir,
                        'out_dir': "output_directory"}
-    config['performance'] = {'num_workers': num_workers,
-                             'max_mem_target_gb': max_mem_target_gb}
-    config['trimming'] = {'type': trimming_type,
-                          'segment_length': trimming_segment_length}
-    config['adapters'] = {'adapter_list': adapter_list,
-                          'adapter_tolerance': adapter_tolerance}
-    config['quality_filter'] = {'minimum_quality': minimum_quality,
+    config['performance'] = {'num_workers': prepared_reads.num_workers,
+                             'max_mem_target_gb':
+                             prepared_reads.max_mem_target_gb}
+    config['trimming'] = {'type': prepared_reads.trimming_type,
+                          'segment_length':
+                          prepared_reads.trimming_segment_length}
+    config['adapters'] = {'adapter_list': prepared_reads.adapter_list,
+                          'adapter_tolerance':
+                          prepared_reads.adapter_tolerance}
+    config['quality_filter'] = {'minimum_quality':
+                                prepared_reads.minimum_quality,
                                 'max_below_threshold':
-                                max_below_threshold}
+                                prepared_reads.max_below_threshold}
 
     with open(path_to_config_file) as readprep_configfile:
         config.write(readprep_configfile)
 
 
-def run_readprep():
+def run_readprep(path_to_config_file, path_to_readprep_main):
     command = ["python", path_to_readprep_main, path_to_config_file]
     subprocess.call(" ".join(command), shell=True)
