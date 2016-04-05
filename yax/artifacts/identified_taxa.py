dd9bb5a7a2415976d138350ff387ac5888de5a48
Mike <mike.deberg@gmail.com>
2016-04-05 15:38:03 -0700

    inital addition of taxanomic identification

---
+++ yax/artifacts/identified_taxa.py
@@ -0,0 +1,16 @@
+from yax.state.type import Artifact
+
+
+class IdentifiedTaxa(Artifact):
+    def __init__(self, completed):
+        self.identified_taxa_file = None
+
+        if completed:
+            self.identified_taxa_file = None
+            read_identified_taxa()
+
+    def read_identified_taxa():
+        pass
+
+    def __complete__(self):
+        pass
---
+++ yax/modules/taxa_identification/taxa_id.py
@@ -0,0 +1,9 @@
+from yax.artifacts.identified_taxa import IdentifiedTaxa
+from yax.state.type.parameter import file_text
+
+
+def main(working_dir,
+         output,
+         runid,
+         parameters) -> IdentifiedTaxa:
+    idedtaxa = output
