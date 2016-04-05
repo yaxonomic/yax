71ea3e460caa852cc0e9dd00eed049ca19188ee5
Mike <mike.deberg@gmail.com>
2016-04-05 16:06:14 -0700

    updating test pipeline

--- yax/state/tests/test_pipeline/modules/module_2.py
+++ yax/state/tests/test_pipeline/modules/module_2.py
@@ -3,7 +3,10 @@ from yax.state.tests.test_pipeline.artifacts.artifact_x import ArtifactX
 from yax.state.type.parameter import Str
 
 
-def main(working_dir, output, details, input_str: Str) -> (ArtifactC, ArtifactX):
+def main(working_dir,
+         output,
+         details,
+         input_str: Str) -> (ArtifactC, ArtifactX):
     art_c, art_x = output
 
     art_c.text = input_str
