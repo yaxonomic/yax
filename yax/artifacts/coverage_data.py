f3ce3673309587f73a1ca6de9a632c943e1f1971
Hayden <haydentwestbrook@gmail.com>
2016-04-09 14:30:44 -0700

    Added sam file parsing to coverage data artifact

--- yax/artifacts/coverage_data.py
+++ yax/artifacts/coverage_data.py
@@ -1,27 +1,70 @@
 from yax.state.type import Artifact
+from os import listdir
+from os.path import isfile, join, splitext
 
 
 class CoverageData(Artifact):
 
     def __init__(self):
         super().__init__(self)
+        self.data_dir = ''
         self.samples = []
 
-    def __complete__(self):
-        self.samples = []
+    def _get_coverage_data(self):
+        """
+        Sets the object representation of coverage data from the collection of
+        sam files.
+        """
+        # Get sam files
+        files = [f for f in listdir(self.data_dir) if isfile(join(self.data_dir
+                                                                  , f)) and
+                 splitext(f)[1].lower() == '.sam']
+
+        # For each file, create a sample object that holds sequences and
+        # alignments
+        for file in files:
+            sample = self.Sample(splitext(file)[0].split('/')[-1])
+            with open(file, 'r') as coverage_file:
+                lines = coverage_file.readlines()
+
+            for line in lines:
+                try:
+                    # If the line is a sequence, add it to the sample's list of
+                    #  sequences
+                    if line.startswith('@SQ'):
+                        tabs = line.split('\t')
+                        gi = tabs[1].split('|')[1]
+                        length = tabs[2].split(':')[1]
+                        sample.sequences.append(self.Sequence(gi, length))
+                    # If the line is an alignment, add it to the sample's list
+                    # of alignments
+                    else:
+                        tabs = line.split('\t')
+                        gi = tabs[2].split('|')[1]
+                        position = tabs[3]
+                        length = len(tabs[9])
+                        sample.alignments.append(self.Alignment(gi, length,
+                                                                position))
+                except Exception:
+                    continue
 
     class Sample:
+        # Holds the sequences listed in the sam file and the alignments to that
+        # sequences
         def __init__(self, name):
             self.name = name
             self.sequences = []
             self.alignments = []
 
     class Sequence:
+        # Holds the gi and length of a sequence
         def __init__(self, gi, length):
             self.gi = gi
             self.length = length
 
     class Alignment:
+        # Holds the gi that was aligned to, length of the alignment, and the
+        # starting position of the alignment on the sequences
         def __init__(self, gi, length, position):
             self.gi = gi
             self.length = length
