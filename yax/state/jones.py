d73143556bd2e62de3f51d1cc2703a5f85b4b9d0
ebolyen <ebolyen@gmail.com>
2016-04-23 19:45:05 -0700

    Add docs and rename methods in artifact_map

--- yax/state/jones.py
+++ yax/state/jones.py
@@ -15,26 +15,55 @@ class Indiana:
 
     @property
     def data_dir(self):
+        """Hidden directory where all internal state will be stored."""
         return os.path.join(self.root_dir, self.DATA_DIR_NAME)
 
     @property
     def artifacts_dir(self):
+        """Directory where all artifacts will be stored."""
         return os.path.join(self.data_dir, 'artifacts')
 
     @property
     def working_dir(self):
+        """Directory where working directories will be stored."""
         return os.path.join(self.data_dir, 'working')
 
     @property
     def arch_path(self):
+        """Filepath to where the architectural configuration will be stored."""
         return os.path.join(self.data_dir, self.ARCH_CONFIG_NAME)
 
     @property
     def artifact_db_path(self):
+        """Filepath to where the sqlite database will be stored."""
         return os.path.join(self.data_dir, self.ARTIFACT_DATABASE_NAME)
 
-    def __init__(self, dir_, pipeline=None):
-        self.root_dir = dir_
+    @property
+    def _default_pipeline(self):
+        """Filepath to the default architectural configuration."""
+        root, _ = os.path.split(os.path.split(__file__)[0])
+        # This is the arch_config module of YAX not ARCH_CONFIG_NAME
+        # DRY does not apply here.
+        return os.path.join(root, 'arch_config.py')
+
+    def __init__(self, root_dir, pipeline=None):
+        """Initialize Indiana, will create .yax dir if not exists.
+
+        Parameters
+        ----------
+        root_dir : path to directory
+            The current (or eventual) location which contains a yax pipeline.
+        pipeline : filepath
+            A path to an architectural configuration file. This can only be
+            provided if `root_dir` is not already a yax pipeline.
+
+        Raises
+        ------
+        ValueError
+            Raised when `pipeline` provided to an initialized yax pipeline.
+
+        """
+        self.root_dir = root_dir
         if not os.path.isdir(self.data_dir):
             self._init_first_time(pipeline)
         elif pipeline is not None:
@@ -46,8 +75,9 @@ class Indiana:
                                self.graph)
 
     def _init_first_time(self, pipeline):
+        """Create the needed subdirectories in .yax"""
         if pipeline is None:
-            pipeline = self._get_default_pipeline()
+            pipeline = self._default_pipeline
         else:
             pipeline = os.path.abspath(pipeline)
         try:
@@ -59,33 +89,29 @@ class Indiana:
             shutil.rmtree(self.data_dir)
             raise
 
-    def _get_default_pipeline(self):
-        root, _ = os.path.split(os.path.split(__file__)[0])
-        # This is the arch_config module of YAX not ARCH_CONFIG_NAME
-        # DRY does not apply here.
-        return os.path.join(root, 'arch_config.py')
-
     def __repr__(self):
         return '\n'.join([repr(node) for node in self.graph])
 
-    def init(self, run_key):
-        self.write_config(run_key)
-
     def prepare(self, run_key):
-        """Prepare a run
+        """Prepare a run given a `run_key`.
 
         Looks for a file in the root directory which matches `run_key`.ini
         The config will be validated and then any corresponding aritfacts
         will be associated or declared with a run.
 
-        Paramters
-        ---------
+        If a run already exists for the given configuration, the artifacts will
+        be declared if necessary and the pre-existing run key will be returned.
+
+        Parameters
+        ----------
         run_key : str
             The run key to use
 
         Returns
         -------
-        None
+        str
+            An empty string if the run was created, otherwise the pre-existing
+            run key.
 
         """
         config = os.path.join(self.root_dir, run_key + '.ini')
@@ -93,13 +119,13 @@ class Indiana:
             raise ValueError("File `%s.ini` does not exist." % run_key)
 
         config = self.read_config(config)
-        run_id, existing_run_key = self.map.create_run(config)
+        run_id, existing_run_key = self.map.declare_run(config)
         self.map.declare_artifacts(config, run_id)
         return existing_run_key
 
     def engage(self, run_key):
-        run_id = self.map.resolve_run_key(run_key)
-        art_fps = self.map.get_artifact_paths(run_id)
+        run_id = self.map.run_key_to_run_id(run_key)
+        art_fps = self.map.bound_artifact_to_filepath(run_id)
 
         for node in self.graph:
             if all([Artifact.declare(art_fps[o], None)
@@ -127,10 +153,33 @@ class Indiana:
             input_artifacts[param] = \
                 param_to_class[param].declare(art_fps[key], node.name)
 
-        input_params = self.map.get_params(run_id, node)
+        input_params = self.map.get_arguments_for_node(node, run_id)
 
         with tempfile.TemporaryDirectory(dir=self.working_dir) as working_dir:
             node(working_dir, outputs, details, input_artifacts, input_params)
+
+    def write_config(self, run_key):
+        fp = os.path.join(self.root_dir, '%s.ini' % run_key)
+        config = configparser.ConfigParser()
+
+        config['details'] = {}
+        for key, value in self.graph.details.items():
+            default = ''
+            if type(value) is tuple:
+                _, default = value
+            config['details'][key] = str(default)
+        config['details']['run_key'] = run_key
+        for node in self.graph:
+            section = {}
+            for key in node.get_input_params():
+                default = ''
+                if key in node.defaults:
+                    default = node.defaults[key]
+                section[key] = default
+            config[node.name] = section
+
+        with open(fp, mode='w') as fh:
+            config.write(fh)
 
     def read_config(self, config_fp):
         config = configparser.ConfigParser()
@@ -186,29 +235,6 @@ class Indiana:
             self._copy_converted_config_value(type_, section_config, section,
                                               key)
         return section
-
-    def write_config(self, run_key):
-        fp = os.path.join(self.root_dir, '%s.ini' % run_key)
-        config = configparser.ConfigParser()
-
-        config['details'] = {}
-        for key, value in self.graph.details.items():
-            default = ''
-            if type(value) is tuple:
-                _, default = value
-            config['details'][key] = str(default)
-        config['details']['run_key'] = run_key
-        for node in self.graph:
-            section = {}
-            for key in node.get_input_params():
-                default = ''
-                if key in node.defaults:
-                    default = node.defaults[key]
-                section[key] = default
-            config[node.name] = section
-
-        with open(fp, mode='w') as fh:
-            config.write(fh)
 
     def _copy_converted_config_value(self, type_, from_, into, key):
         try:
